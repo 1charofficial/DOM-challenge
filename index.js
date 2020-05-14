fc745f6a6358772408c0d2a83ea2b0376c74f693
Charmaine Connors <codenationstudfent@codes-mbp.home>
2020-05-14 08:19:00 +0100

    second commit with updates to HTML, CSS, JS

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
-const wrapper = document.getElementById('wrapper');
-const box = document.getElementById('box');
-//box.id = 'box';
 
-const heading = getElementById('heading');
+//document.querySelector("h1").innerHTML = "Enter a search term and a number, let's look at some cool images!"
+//const theHeading = document.getElementById("theHeading");
 
-heading.addEventListener("click", () => {
-    heading.style.color = "black";
-})
+//theHeading.addEventListener("click", () => {
+// theHeading.style.color = "purple";
 
-const input = document.getElementById("input");
-const button = document.getElementById("button");
-
-button.addEventListener("click", () => {
-    button.style.color = "red"; 
-    box.style.color = input.Value;
+//}) - I will not need the heading at the moment, however I want to keep this code as I have some plans to 
+//come back to the project and extend it / add more code, more functionality etc..
 
 
-    
-})// why is this not working now?
 
-wrapper.appendChild('div')
+const numOfImg = document.getElementById("numberOf");
+const typeOfImg = document.getElementById("typeOf");
+const makeBox = document.getElementById("makeBox");
+const clearBox = document.getElementById("clearBox");
+const wrapper = document.getElementById("wrapper");
 
 
-button.onclick = () => {
-    button.id.toggle('hide');
+
+
+
+const createImg = (numOfImg, typeOfImg) =>{
+
+  for(let i =0; i < numOfImg; i++);
+  {
+    let image = document.createElement('img');
+    image.src = `https://source.unsplash.com/random/500x500/?sig=${numOfImg}&${typeOfImg}`;
+    let image = document.getElementById("box");
+  }
+  
+  wrapper.appendChild("box");
+  
+
+
 }
 
-//div.appendChild(button);
-
-//div.appendChild(text); - create textNode?
-
-//console.log(div);
 
 
+const clearScreen = (clearBox) => {
+  
+  clearBox.document("click", () => {
+    while(wrapper.hasChildNodes()) {
+      wrapper.removeChild(wrapper.lastChild);
+    }
+  })
+}
+
+//This function will be used to clear the screen and start again.
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//I need to produce a number of boxes (pre-determined). 
+
+//I then need to be able to produce boxes on request of the user.
+
+//I need to then let the user request a type of image as well as a number of images.
+
+//I need to connect the number of things and the type of things to the image generator, im thinking of a loop, that can run the function 
+//a number of times based on the users request.
+
+//a function to feed back images based on two queries
+
+//a function to make the feeding of images happen - event - on click
+
+//a function to clear the screen - an event - onclick
+
+//a function to re start the function that took the queries so it starts from a fresh
--- readme.md
+++ readme.md
@@ -7,3 +7,21 @@ Level 3 - Allow the user to clear all boxes via a clear boxes button.
 
 *Add images dynamically to boxes using unsplash query:  https://source.unsplash.com/random/500x500/?sig=<randomNum>&<query> *bold needs modifying*
 
+//I need to produce a number of boxes (pre-determined). 
+
+//I then need to be able to produce boxes on request of the user.
+
+//I need to then let the user request a type of image as well as a number of images.
+
+//I need to connect the number of things and the type of things to the image generator, im thinking of a loop, that can run the function 
+//a number of times based on the users request.
+
+//a function to feed back images based on two queries
+
+//a function to make the feeding of images happen - event - on click
+
+//a function to clear the screen - an event - onclick
+
+//a function to re start the function that took the queries so it starts from a fresh
+
+//
